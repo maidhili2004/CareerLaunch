1ee0304842c210166678357c5cfedf5f5c761951
vimu <vimu@Manognas-MacBook-Air.local>
2025-08-01 20:27:29 +0530

    ✅ Fix: Removed invalid @tailwindcss/postcss dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
-const tailwindcss = require('@tailwindcss/postcss');
-
 module.exports = {
-  plugins: [
-    tailwindcss,
-    require('autoprefixer'),
-  ],
-};
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+  
 
 
   
